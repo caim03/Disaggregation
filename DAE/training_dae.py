d636006be53c4efeeb3dc08a8b3597395cbd2419
caim03 <christian.lariccia@gmail.com>
2018-10-31 11:47:45 +0100

    update

--- DAE/training_dae.py
+++ DAE/training_dae.py
@@ -10,47 +10,49 @@ from nilmtk.datastore import HDFDataStore
 
 #from nilmtk.dataset_converters import convert_ukdale
 #convert_ukdale('./data/UKDALE', './data/UKDALE/ukdale.h5')  # Skip if we already have the data in .h5 file
-
-SEQUENCE = 256
+DATASET = '../data/ENEA/enea.h5'
+MODEL = '../data/ENEA/model-dae-enea.h5'
+DISAG = '../data/ENEA/disag-dae-out.h5'
+APPLIANCE = 'fridge'
 TRAIN_BUILDING = 1
-TEST_BUILDING = 2
+TEST_BUILDING = 1
+SEQUENCE = 256
 
-train = DataSet('../data/UKDALE/ukdale.h5')
-train.set_window(start="2013-04-12", end="2015-07-01") # Training data time window
+train = DataSet(DATASET)
+train.set_window(start="2017-03-11", end="2017-08-31") # Training data time window
 train_elec = train.buildings[TRAIN_BUILDING].elec # Get building 1 meters
 
 dae = DAEDisaggregator(SEQUENCE)
 
 train_mains = train_elec.mains() # The aggregated meter that provides the input
-train_meter = train_elec.submeters()['fridge'] # The kettle meter that is used as a training target
+train_meter = train_elec.submeters()[APPLIANCE] # The kettle meter that is used as a training target
 
 print("------ TRAINING ------")
-dae.train(train_mains, train_meter, epochs=50, sample_period=6)
-dae.export_model("../data/UKDALE/dae-ukdale.h5")
+dae.train(train_mains, train_meter, epochs=50, sample_period=1)
+dae.export_model(MODEL)
 
 # dae.import_model("../data/UKDALE/dae-ukdale.h5")
-test = DataSet('../data/UKDALE/ukdale.h5')
-test.set_window(start="2013-05-22", end="2013-09-24")
+test = DataSet(DATASET)
+test.set_window(start="2013-09-01", end="2013-10-31")
 test_elec = test.buildings[TEST_BUILDING].elec
 test_mains = test_elec.mains()
 
-disag_filename = '../data/UKDALE/disag-dae-out.h5' # The filename of the resulting datastore
 from nilmtk.datastore import HDFDataStore
-output = HDFDataStore(disag_filename, 'w')
+output = HDFDataStore(DISAG, 'w')
 
 print("------ TESTING ------")
-dae.disaggregate(test_mains, output, train_meter, sample_period=6)
+dae.disaggregate(test_mains, output, train_meter, sample_period=1)
 
-result = DataSet(disag_filename)
+result = DataSet(DISAG)
 res_elec = result.buildings[TEST_BUILDING].elec
-predicted = res_elec['fridge']
-ground_truth = test_elec['fridge']
+predicted = res_elec[APPLIANCE]
+ground_truth = test_elec[APPLIANCE]
 
 fig = plt.figure()
 ax = plt.subplot(111)
 ax.plot(predicted.power_series_all_data(), label='predicted')
 ax.plot(ground_truth.power_series_all_data(), label='ground truth')
-plt.xlim('2013-06-22 00:00:00', '2013-06-22 23:59:00')
+#plt.xlim('2013-06-22 00:00:00', '2013-06-22 23:59:00')
 plt.xlabel('Time [Hours]')
 plt.ylabel('Power [W]')
 plt.title('Fridge Disaggregation')
--- RNN/training_rnn.py
+++ RNN/training_rnn.py
@@ -10,65 +10,55 @@ from nilmtk.datastore import HDFDataStore
 
 #from nilmtk.dataset_converters import convert_ukdale
 #convert_ukdale('./data/UKDALE', './data/UKDALE/ukdale.h5')  # Skip if we already have the data in .h5 file
-
+DATASET = '../data/ENEA/enea.h5'
+MODEL = '../data/ENEA/model-lstm-enea.h5'
+DISAG = '../data/ENEA/disag-lstm-out.h5'
+APPLIANCE = 'fridge'
 TRAIN_BUILDING = 1
-TEST_BUILDING = 2
+TEST_BUILDING = 1
 
-train = DataSet('../data/UKDALE/ukdale.h5')
-train.set_window(start="2013-04-12", end="2015-07-01") # Training data time window
+train = DataSet(DATASET)
+train.set_window(start="2017-03-11", end="2017-08-31") # Training data time window
 train_elec = train.buildings[TRAIN_BUILDING].elec # Get building 1 meters
 
 rnn = RNNDisaggregator()
 
 train_mains = train_elec.mains() # The aggregated meter that provides the input
-train_meter = train_elec.submeters()['fridge'] # The kettle meter that is used as a training target
-"""
-print("------ TRAINING ------")
-rnn.train(train_mains, train_meter, epochs=5, sample_period=6)
-rnn.export_model("../data/UKDALE/model-ukdale.h5")
-"""
-"""
-rnn.import_model("../data/UKDALE/model-ukdale.h5")
-test = DataSet('../data/UKDALE/ukdale.h5')
-test.set_window(start="2013-05-22", end="2013-09-24")
+train_meter = train_elec.submeters()[APPLIANCE] # The kettle meter that is used as a training target
+
+#print("------ TRAINING ------")
+#rnn.train(train_mains, train_meter, epochs=5, sample_period=1)
+#rnn.export_model(MODEL)
+
+rnn.import_model(MODEL)
+test = DataSet(DATASET)
+test.set_window(start="2013-09-01", end="2013-10-31")
 test_elec = test.buildings[TEST_BUILDING].elec
 test_mains = test_elec.mains()
 
-disag_filename = '../data/UKDALE/disag-out.h5' # The filename of the resulting datastore
-from nilmtk.datastore import HDFDataStore
-output = HDFDataStore(disag_filename, 'w')
+output = HDFDataStore(DISAG, 'w')
 
 print("------ TESTING ------")
-rnn.disaggregate(test_mains, output, train_meter, sample_period=6)
-"""
+rnn.disaggregate(test_mains, output, train_meter, sample_period=1)
 
-test = DataSet('../data/UKDALE/ukdale.h5')
-test.set_window(start="2013-05-22", end="2013-09-24")
-test_elec = test.buildings[TEST_BUILDING].elec
-test_mains = test_elec.mains()
-
-disag_filename = '../data/UKDALE/disag-out.h5' # The filename of the resulting datastore
-
-result = DataSet(disag_filename)
+result = DataSet(DISAG)
 res_elec = result.buildings[TEST_BUILDING].elec
-predicted = res_elec['fridge']
-ground_truth = test_elec['fridge']
+predicted = res_elec[APPLIANCE]
+ground_truth = test_elec[APPLIANCE]
 
 fig = plt.figure()
 ax = plt.subplot(111)
 ax.plot(predicted.power_series_all_data(), label='predicted')
 ax.plot(ground_truth.power_series_all_data(), label='ground truth')
-plt.xlim('2013-06-22 00:00:00', '2013-06-22 03:00:00')
-plt.xlabel('Time [Hours]')
+#plt.xlim('2013-06-22 00:00:00', '2013-06-22 03:00:00')
+plt.xlabel('Time')
 plt.ylabel('Power [W]')
 plt.title('Fridge Disaggregation')
 myFmt = mdates.DateFormatter('%H:%M')
 ax.xaxis.set_major_formatter(myFmt)
 ax.legend()
-plt.savefig("fridge.png")
+plt.savefig("fridge_lstm.png")
 
-"""
 import metrics
 print("============ Relative error in total energy: {}".format(metrics.relative_error_total_energy(predicted, ground_truth)))
 print("============ Mean absolute error(in Watts): {}".format(metrics.mean_absolute_error(predicted, ground_truth)))
-"""
