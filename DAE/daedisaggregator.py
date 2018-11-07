398bcc29748235b0d630e23e84891dcd751ea3d5
caim03 <christian.lariccia@gmail.com>
2018-11-07 14:53:54 +0100

    new model

--- DAE/daedisaggregator.py
+++ DAE/daedisaggregator.py
@@ -348,7 +348,7 @@ class DAEDisaggregator(Disaggregator):
 
         # Fully Connected Layers
         #model.add(Dropout(0.1))
-        model.add(Dense((sequence_len-0)*8, activation='relu',
+        model.add(Dense((sequence_len-0)*8, activation='relu',input_shape=(sequence_len, 1),
         kernel_initializer=initializers.RandomNormal(mean=0.1, stddev=0.1)))#provare con linear sui dense
 
         model.add(Dropout(0.1))
@@ -356,7 +356,7 @@ class DAEDisaggregator(Disaggregator):
         kernel_initializer=initializers.RandomNormal(mean=0.1, stddev=0.1)))
 
         model.add(Dropout(0.1))
-        model.add(Dense((sequence_len-0)*8, activation='relu',
+        model.add(Dense((sequence_len-0)*8, activation='relu',input_shape=(sequence_len, 1),
         kernel_initializer=initializers.RandomNormal(mean=0.1, stddev=0.1)))
 
         #model.add(Dropout(0.1))
