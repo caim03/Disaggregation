1188311c4e9df6d1a6c5d2b8247f8c12773d420d
caim03 <christian.lariccia@gmail.com>
2018-11-07 13:13:29 +0100

    new model

--- DAE/training_dae.py
+++ DAE/training_dae.py
@@ -54,7 +54,7 @@ train_meter = train_elec.submeters()[APPLIANCE] # The kettle meter that is used 
 
 if TRAINING:
     print("------ TRAINING ------")
-    dae.train(train_mains, train_meter, epochs=10, sample_period=1)
+    dae.train(train_mains, train_meter, epochs=3, sample_period=1)
     dae.export_model(MODEL)
 else:
     print("------ IMPORT MODEL ------")
